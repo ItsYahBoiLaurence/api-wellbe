ebe481574e7099fcf7a169a65f7bbbcdeaf1f8a6
ItsYahBoiLaurence <laurence@mayan.com.ph>
2025-04-14 10:41:17 +0800

    User Registration Completed

--- src/modules/employee/user/user.service.ts
+++ src/modules/employee/user/user.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, ConflictException, Injectable } from '@nestjs/common';
+import { BadRequestException, ConflictException, Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/modules/prisma/prisma.service';
 import { UserModel } from 'src/types/user';
 
@@ -7,15 +7,34 @@ export class UserService {
     constructor(private readonly prisma: PrismaService) { }
 
     async createEmployee(payload: UserModel) {
-        if (!payload) throw new BadRequestException("Invalid Payload")
+        try {
+            if (!payload) throw new BadRequestException("Invalid Payload")
 
-        const company = await this.prisma.department.findFirst({
-            where: {
-                name: payload.department_name,
-                company: {
-                    name: payload.company
+            const department = await this.prisma.department.findFirst({
+                where: {
+                    name: payload.department_name,
+                    company: {
+                        name: payload.company
+                    }
                 }
-            }
-        })
+            })
+
+            if (!department) throw new NotFoundException('Company does not exist!')
+
+            const newUser = this.prisma.employee.create({
+                data: {
+                    email: payload.email,
+                    first_name: payload.first_name,
+                    last_name: payload.last_name,
+                    department_id: department.id
+                }
+            })
+
+            if (!newUser) throw new ConflictException('User creation failed!')
+
+            return newUser
+        } catch (error) {
+            throw error
+        }
     }
 }
