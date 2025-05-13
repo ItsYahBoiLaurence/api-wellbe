f56935cc3a402444d42fbfa31f4362cd52e56313
ItsYahBoiLaurence <laurence@mayan.com.ph>
2025-05-13 15:04:28 +0800

    Cron String

--- src/modules/cron/cron.service.ts
+++ src/modules/cron/cron.service.ts
@@ -41,7 +41,7 @@ export class CronService implements OnModuleInit {
         if (!companies) throw new Exception("No Companies")
 
         companies.map(({ company_name, employees_under_batch, frequency }) => {
-            const cron_string = frequency === "DAILY" ? "*/10 * * * * *" : "*/5 * * * * *"
+            const cron_string = frequency === "DAILY" ? "* * * * *" : "*/5 * * * * *"
             const emails = employees_under_batch.map(emp => emp.email)
             this.addCronJob(company_name, emails, cron_string)
         })
@@ -53,57 +53,57 @@ export class CronService implements OnModuleInit {
     }
 
     private async startCompanyCronJob(company_name: string, emails: string[]) {
-        // const company = await this.helper.getCompany(company_name)
-        // const batch = await this.helper.getLatestBatch(company.name)
+        const company = await this.helper.getCompany(company_name)
+        const batch = await this.helper.getLatestBatch(company.name)
 
 
-        // if (batch?.current_set_number >= 5) {
-        //     await this.prisma.batch_Record.update({
-        //         where: {
-        //             id: batch.id
-        //         },
-        //         data: {
-        //             is_completed: true
-        //         }
-        //     })
-        //     this.scheduleRegistry.deleteCronJob(`${this.stringTransformer(company_name)}-company-job`)
-        //     Logger.log("The Batch ended!")
-        //     return
-        // }
+        if (batch?.current_set_number >= 5) {
+            await this.prisma.batch_Record.update({
+                where: {
+                    id: batch.id
+                },
+                data: {
+                    is_completed: true
+                }
+            })
+            this.scheduleRegistry.deleteCronJob(`${this.stringTransformer(company_name)}-company-job`)
+            Logger.log("The Batch ended!")
+            return
+        }
 
-        // Logger.log("===========")
-        // Logger.log(`Questions were sent for the Batch ${batch.current_set_number + 1}`)
-        // Logger.log("===========")
+        Logger.log("===========")
+        Logger.log(`Questions were sent for the Batch ${batch.current_set_number + 1}`)
+        Logger.log("===========")
 
-        // Logger.log("===========")
-        // Logger.log(emails)
-        // Logger.log("===========")
+        Logger.log("===========")
+        Logger.log(emails)
+        Logger.log("===========")
 
-        // const left = `${(5 - (batch.current_set_number + 1)) == 0 ? "no" : (5 - (batch.current_set_number + 1))}`
-        // emails.map(async (email) => {
-        //     const user = await this.helper.getUserByEmail(email)
-        //     if (!user) Logger.log('No user')
-        //     const email_data = {
-        //         to: email,
-        //         subject: "Wellbe Reminder",
-        //         company: company.name,
-        //         user: user?.first_name as string,
-        //         left
-        //     }
+        const left = `${(5 - (batch.current_set_number + 1)) == 0 ? "no" : (5 - (batch.current_set_number + 1))}`
+        emails.map(async (email) => {
+            const user = await this.helper.getUserByEmail(email)
+            if (!user) Logger.log('No user')
+            const email_data = {
+                to: email,
+                subject: "Wellbe Reminder",
+                company: company.name,
+                user: user?.first_name as string,
+                left
+            }
 
-        //     try {
-        //         await this.emailer.reminderEmail(email_data)
-        //     } catch (error) {
-        //         Logger.log(`Error sending email to ${email}`)
-        //         Logger.log(`error`)
-        //     }
-        // })
+            try {
+                await this.emailer.reminderEmail(email_data)
+            } catch (error) {
+                Logger.log(`Error sending email to ${email}`)
+                Logger.log(`error`)
+            }
+        })
 
-        // await this.prisma.batch_Record.update({
-        //     where: { id: batch?.id },
-        //     data: { current_set_number: batch?.current_set_number + 1 }
-        // })
-        Logger.log(`${company_name}`)
+        await this.prisma.batch_Record.update({
+            where: { id: batch?.id },
+            data: { current_set_number: batch?.current_set_number + 1 }
+        })
+        // Logger.log(`${company_name}`)
     }
 
     addCronJob(company: string, emails: string[], cronString: string) {
